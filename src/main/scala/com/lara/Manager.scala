0d4c775ac01de414bea9b292339467c381c7ce81
hari <hari.harmalkar@gmail.com>
2017-11-21 16:46:17 +0530

    kept imports at top

--- src/main/scala/com/lara/Manager.scala
+++ src/main/scala/com/lara/Manager.scala
@@ -3,9 +3,11 @@ package com.lara
 import java.sql.Connection
 import java.util.Date
 
-import anorm._
-import anorm._
-import anorm.SqlParser._
+import anorm.SqlQuery
+import anorm.SQL
+import anorm.~
+import anorm.SqlParser.{int, str};
+import anorm.ResultSetParser
 import scalikejdbc.ConnectionPool
 import scalikejdbc.config.DBs
 
@@ -18,18 +20,14 @@ object Manager {
   val sql: SqlQuery = SQL("select * from products order by name asc")
 
   def main(args: Array[String]): Unit = {
-    //Class.forName("com.mysql.jdbc.Driver")
-    // ConnectionPool.singleton("jdbc:mysql://localhost:3306/test", "root", "agreeya@123")
     DBs.setupAll()
-    getAllWithParser.foreach(x=>println(x.description))
+    getAllWithParser.foreach(x => println(x))
   }
 
   import anorm.RowParser
 
   val productParser: RowParser[Product] = {
-    import anorm.~
-    import anorm.SqlParser._
-      int("id") ~
+    int("id") ~
       str("name") ~
       str("description") map {
       case id ~ name ~ description =>
@@ -37,7 +35,7 @@ object Manager {
     }
   }
 
-  import anorm.ResultSetParser
+
 
   val productsParser: ResultSetParser[List[Product]] = {
     productParser *
@@ -52,7 +50,6 @@ object Manager {
   object DB {
     def withConnection[A](block: Connection => A): A = {
       val connection: Connection = ConnectionPool.borrow()
-
       try {
         block(connection)
       } finally {
